8ac4d516d0a3f359525ebc07f22d34282f70c8c9
sun4csit <sunil6ptr@gmail.com>
2023-02-11 23:18:00 +0530

    Get product list
    1. Get list of product and filter
    2. Added Login template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <nav class='navbar navbar-expand navbar-dark bg-dark'>
+    <a class='navbar-brand'>{{pageTitle}}</a>
+
+    <ul class="navbar-nav ml-auto" >
+    <li><a class='nav-link' routerLink='/welcome'>Home</a></li>
+      <li class="nav-item">
+        <a href="/register" class="nav-link" routerLink="register">Sign Up</a>
+      </li>
+      <li class="nav-item">
+        <a href="/login" class="nav-link" routerLink="login">Login</a>
+      </li>
+
+      <li><a class='nav-link' routerLink='/products'>Product List</a></li>
+
+    </ul>
+  </nav>
+  <div class='container'>
+    <router-outlet></router-outlet>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Product Management';
 }
